11c4d5f702c23c436297f000595b041bbafe65da
LightBear124 <nazarkak468@gmail.com>
2025-12-12 13:33:46 +0700

    Добавлена функция умножения

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ def add(a, b):
     return a + b
 
 
+def multiply(a, b):
+    return a * b
+
+
 def main():
     print("Hello, World!")
     print("2 + 3 =", add(2, 3))
+    print("2 * 3 =", multiply(2, 3))
 
 
 if __name__ == "__main__":
